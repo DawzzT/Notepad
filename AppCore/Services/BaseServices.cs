540862ccb8f5893c656f8a17298b85d1539b09ac
Walter Díaz <walddt05@gmail.com>
2022-03-14 18:29:52 -0600

    Fin appCore

---
+++ AppCore/Services/BaseServices.cs
@@ -0,0 +1,33 @@
+﻿using AppCore.IServices;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Services
+{
+    public abstract class BaseServices<T> : IServices<T>
+    {
+        private IModel<T> model;
+        protected BaseServices(IModel<T> _model)
+        {
+            this.model = _model;
+        }
+        public T Create(string n)
+        {
+            return model.Create(n);
+        }
+
+        public void OverWrite(string path, string text)
+        {
+            model.OverWrite(path, text);
+        }
+
+        public string Read(string path)
+        {
+            return model.Read(path);
+        }
+    }
+}
---
+++ AppCore/Services/DirectoryServices.cs
@@ -0,0 +1,34 @@
+﻿using AppCore.IServices;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Services
+{
+    public class DirectoryServices : BaseServices<DirectoryInfo>, IDirectoryServices
+    {
+        IDirectory repository;
+        public DirectoryServices(IDirectory _repository) : base(_repository)
+        {
+            this.repository = _repository;
+        }
+        public DirectoryInfo CreateFile(string path, string name)
+        {
+            return repository.CreateFile(path, name);
+        }
+
+        public void Delete(string path)
+        {
+            repository.Delete(path);
+        }
+
+        public DirectoryInfo get(string path)
+        {
+            return repository.get(path);
+        }
+    }
+}

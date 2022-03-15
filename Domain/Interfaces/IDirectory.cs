d49a2c17aafa849446ceacbc39392043da2d0cfc
Walter Díaz <walddt05@gmail.com>
2022-03-14 18:20:47 -0600

    Agregar archivos de proyecto.

---
+++ Domain/Interfaces/IDirectory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IDirectory : IModel<DirectoryInfo>
+    {
+        void Delete(string path);
+
+        DirectoryInfo get(string path);
+
+        DirectoryInfo CreateFile(string path, string name);
+    }
+}

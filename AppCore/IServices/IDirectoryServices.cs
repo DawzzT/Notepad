8b3a8bf4495141e645b6bfabc1b0196b1fe7e2e6
Walter Díaz <walddt05@gmail.com>
2022-03-14 18:27:14 -0600

    AappCore

---
+++ AppCore/IServices/IDirectoryServices.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.IServices
+{
+    interface IDirectoryServices : IServices<DirectoryInfo>
+    {
+        void Delete(string path);
+
+        DirectoryInfo get(string path);
+
+        DirectoryInfo CreateFile(string path, string name);
+    }
+}
---
+++ AppCore/IServices/IServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.IServices
+{
+    public interface IServices<T>
+    {
+        T Create(string n);
+        string Read(string path);
+        void OverWrite(string path, string text);
+    }
+}
